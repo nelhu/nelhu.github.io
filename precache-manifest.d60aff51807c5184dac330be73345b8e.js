d4cf69153673f8e476ab41e6106bb19fe11ab67b
huxuezi <huxuezi@huohua.cn>
2019-04-19 16:52:52 +0800

    paste produaction code

---
+++ precache-manifest.d60aff51807c5184dac330be73345b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ab3b22936f891f504c",
+    "url": "/static/css/main.ceec0be9.chunk.css"
+  },
+  {
+    "revision": "99ab3b22936f891f504c",
+    "url": "/static/js/main.f7c6a0df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "221be964be0be7fbf9be7c89c49889b4",
+    "url": "/index.html"
+  }
+];
